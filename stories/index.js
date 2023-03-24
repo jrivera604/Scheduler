9da6f43ea06a3e12644da7820f4bafade6b08ee1
Josh Rivera <joshrivera91@hotmail.com>
2023-03-23 23:35:36 -0700

    Add Error Status, and confirm

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,9 @@ import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 storiesOf("Button", module)
   .addParameters({
@@ -147,11 +150,26 @@ backgrounds: [{ name: "white", value: "#fff", default: true }]
 .add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => <Header time="12pm" />)
 .add("Empty", () => <Empty onAdd={action('onAdd')} />)
-.add('Show', () => (
+.add("Show", () => (
   <Show
     student="Lydia Miller-Jones"
     interviewer={interviewer}
     onEdit={action('onEdit')}
     onDelete={action('onDelete')}
+  />
+))
+.add("Confirm", () => (
+  <Confirm 
+    message="Delete the appointment?"
+    onConfirm={action('onConfirm')}
+    onCancel={action('onCancel')}
+  />
+))
+
+.add("Status", () => <Status message="Deleting" />)
+.add('Error', () => (
+  <Error
+    message="Could not delete appointment."
+    onClose={action('onClose')}
   />
 ));
